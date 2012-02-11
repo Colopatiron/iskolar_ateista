655d6b565057369f9da272616b373fdb41621ec9
Jem Pimentel <colopatiron2009@hotmail.com>
2012-02-11 22:54:44 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IskolarAteista::Application.config.secret_token = 'b9fa0a1fbf1e3f859c5623722a064250872e7f0d30be23b2ac15ed52e3b9557c3ac3f307eaa688fb3de93009c1f7dae61673dc330e15a8cccb30546a6293ddda'
