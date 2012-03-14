730e2b8fed24297f48a2ed0349de7152f6c4867f
Jem Pimentel <colopatiron2009@hotmail.com>
2012-03-15 00:39:05 +0800

    Deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,65 +4,126 @@ Product.create(title: 'The God Delusion',
   author: 'Richard Dawkins',
   description: 
     %{<p>
-        <em>Lorem ipsum dolor sit amet</em>, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        In <em>The God Delusion</em>, Dawkins contends that a supernatural creator almost certainly does not exist and that belief in a personal god qualifies as a delusion, which he defines as a persistent false belief held in the face of strong contradictory evidence. He is sympathetic to Robert Pirsig's statement in Lila that "when one person suffers from a delusion it is called insanity. When many people suffer from a delusion it is called religion."
       </p>},
-  image_url:   'cs.jpg',    
+  image_url:   'goddelusion.JPG',    
   price: 326.00)
 
 Product.create(title: 'The End of Faith',
   author: 'Sam Harris',
   description:
     %{<p>
-        <em>Mauris facilisis</em>, nibh vitae commodo congue, nulla lacus bibendum quam, at pretium urna erat ullamcorper diam. Nullam fermentum mi ut elit elementum pulvinar. Phasellus congue pretium dui, sit amet posuere metus aliquet quis. Phasellus sollicitudin justo sit amet velit mollis quis faucibus risus egestas.
+        <em>The End of Faith: Religion, Terror, and the Future of Reason</em> is a book by Sam Harris, concerning organized religion, the clash between religious faith and rational thought, and the problems of tolerance towards religious fundamentalism.
       </p>},
-  image_url: 'ruby.jpg',
-  price: 849.95)
+  image_url: 'endoffaith.JPG',
+  price: 549.95)
 
 
 Product.create(title: 'I Sold My Soul on Ebay',
   author: 'Hemant Metha',
   description: 
     %{<p>
-        <em>Aenean pellentesque</em>, massa et imperdiet laoreet, risus massa fermentum quam, sit amet scelerisque lacus justo nec quam. Aliquam felis lacus, euismod vel gravida vitae, molestie at velit. Phasellus vestibulum tortor at metus interdum non rhoncus velit commodo. Fusce euismod, enim in adipiscing posuere, tellus eros tincidunt magna, sit amet pharetra ligula magna ac nisl. Aliquam elementum tellus eu dui porttitor eu pretium tellus mattis.
+        <em>I Sold My Soul on eBay</em> is a non-fiction book by Hemant Mehta, an atheist and blogger, who describes his visits to a variety of Christian churches. These visits initially occurred as a result of an eBay auction Mehta created where he offered to visit the worship services of the winning bidder's choosing. The media later branded this auction as Mehta "selling his soul."
       </p>},
-  image_url: 'rtp.jpg',
+  image_url: 'soldmysoul.JPG',
   price: 134.95)
 
-Product.create(title: 'CoffeeScript',
-  author: 'John Doe',
+Product.create(title: 'The Moral Landscape',
+  author: 'Sam Harris',
   description: 
     %{<p>
-        CoffeeScript is JavaScript done right. It provides all of JavaScript's
-	functionality wrapped in a cleaner, more succinct syntax. In the first
-	book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript
-	while writing clearer, cleaner, and safer code.
+        <em>The Moral Landscape: How Science Can Determine Human Values</em> is a book by Sam Harris. In it, he promotes a science of morality and argues that many thinkers have long confused the relationship between morality, facts, and science. He aims to carve a third path between secularists who say morality is subjective, and religionists who say that morality is given by God and scripture.
       </p>},
-  image_url:   'cs.jpg',    
-  price: 1036.00)
+  image_url:   'moral.JPG',    
+  price: 250.50)
 
-Product.create(title: 'Programming Ruby 1.9',
-  author: 'Jane Doe',
+Product.create(title: 'Pale Blue Dot',
+  author: 'Carl Sagan',
   description:
     %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
+        <em>Pale Blue Dot: A Vision of the Human Future in Space</em> is a non-fiction book by Carl Sagan. It is the sequel to <em>Cosmos</em> and was inspired by the "Pale Blue Dot" photograph, for which Sagan provides a sobering description. In this book, Sagan mixes philosophy about the human place in the universe with a description of the current knowledge about the Solar System. He also details a human vision for the future.
       </p>},
-  image_url: 'ruby.jpg',
-  price: 49.95)
+  image_url: 'paleblue.JPG',
+  price: 423.75)
 
 
-Product.create(title: 'Rails Test Prescriptions',
-  author: 'Jimmy Doe',
+Product.create(title: 'A Brief History of Time',
+  author: 'Stephen Hawking',
   description: 
     %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-        Rails applications, covering Test-Driven Development from both a
-        theoretical perspective (why to test) and from a practical perspective
-        (how to test effectively). It covers the core Rails testing tools and
-        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+        <em>A Brief History of Time</em> is a popular-science book written by British physicist Stephen Hawking. It became a best-seller and has sold more than 10 million copies. It was also on the London Sunday Times best-seller list for more than four years.
       </p>},
-  image_url: 'rtp.jpg',
-  price: 34.95)
+  image_url: 'briefhistory.JPG',
+  price: 175.25)
+
+Product.create(title: 'Cosmos',
+  author: 'Carl Sagan',
+  description: 
+    %{<p>
+        <em>Cosmos</em> is a popular science book by astronomer and Pulitzer Prize-winning author Carl Sagan. Its 13 illustrated chapters, corresponding to the 13 episodes of the Cosmos TV series on which the book was based, explore the mutual development of science and civilization.
+      </p>},
+  image_url: 'cosmos.JPG',
+  price: 630.99)
+
+Product.create(title: 'The Grand Design',
+  author: 'Stephen Hawking',
+  description: 
+    %{<p>
+        <em>The Grand Design</em> is a popular-science book written by physicists Stephen Hawking and Leonard Mlodinow. It argues that invoking God is not necessary to explain the origins of the universe, and that the Big Bang is a consequence of the laws of physics alone. In response to criticism, Hawking has said; "One can't prove that God doesn't exist, but science makes God unnecessary."
+      </p>},
+  image_url: 'granddesign.JPG',
+  price: 284.40)
+  
+Product.create(title: 'The Magic of Reality',
+  author: 'Richard Dawkins',
+  description: 
+    %{<p>
+        <em>The Magic of Reality: How We Know What's Really True</em> is a 2011 book by British biologist Richard Dawkins. It is a graphic science book aimed primarily at children and young adults. Dawkins has stated that the book is intended for those aged around 12 years and upwards, and that when trialling the book prior to publishing, younger readers were able to understand its content with additional adult assistance.
+      </p>},
+  image_url: 'magic.JPG',
+  price: 989.99)
+  
+Product.create(title: 'Letter to a Christian Nation',
+  author: 'Sam Harris',
+  description: 
+    %{<p>
+        <em>Letter to a Christian Nation</em> is a non-fiction book by Sam Harris, written in response to feedback he received following the publication of his first book The End of Faith. The book is written in the form of an open letter to a Christian in the United States. Harris states that his aim is "to demolish the intellectual and moral pretensions of Christianity in its most committed forms."
+      </p>},
+  image_url: 'letter.JPG',
+  price: 139.95)
+  
+Product.create(title: 'God, No!',
+  author: 'Penn Jillette',
+  description: 
+    %{<p>
+        <em>God, No! Signs You May Already Be An Atheist and Other Magical Tales</em> is a book by illusionist and comedian Penn Jillette. Described as an atheist bible of sorts by reviewers, the book includes an atheist's take on the 10 commandments.
+      </p>},
+  image_url: 'godno.JPG',
+  price: 350.75)
+  
+Product.create(title: 'The Demon-Haunted World',
+  author: 'Carl Sagan',
+  description: 
+    %{<p>
+        <em>The Demon-Haunted World: Science as a Candle in the Dark</em> is a book by astrophysicist Carl Sagan which intends to explain the scientific method to laypeople, and to encourage people to learn critical or skeptical thinking. It explains methods to help distinguish between ideas that are considered valid science, and ideas that can be considered pseudoscience. Sagan states that when new ideas are offered for consideration, they should be tested by means of skeptical thinking, and should stand up to rigorous questioning.
+      </p>},
+  image_url: 'demon.JPG',
+  price: 789.99)
+  
+Product.create(title: 'Arguably: Essays',
+  author: 'Christopher Hitchens',
+  description: 
+    %{<p>
+        <em>Arguably: Essays</em> is a 2011 book by Christopher Hitchens, comprising 107 essays on a variety of political and cultural topics.
+      </p>},
+  image_url: 'arguably.JPG',
+  price: 455.00)
+  
+Product.create(title: 'God is Not Great',
+  author: 'Christopher Hitchens',
+  description: 
+    %{<p>
+        <em>God Is Not Great: How Religion Poisons Everything</em> is a 2007 book by the late author and journalist Christopher Hitchens criticising religion. He contends that organised religion is "violent, irrational, intolerant, allied to racism, tribalism, and bigotry, invested in ignorance and hostile to free inquiry, contemptuous of women and coercive toward children" and sectarian, and that accordingly it "ought to have a great deal on its conscience." 
+      </p>},
+  image_url: 'godisnotgreat.JPG',
+  price: 623.95)
